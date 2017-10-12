d6bd5a8d16e13c39c51b532998f7e5509fc45a69
veerarrabdullah786 <31401798+veerarrabdullah786@users.noreply.github.com>
2017-10-12 12:37:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('Article-one', function (req, res); {
+app.get('Article-one', function (req, res) {
   res.send('Article one are requested and will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
