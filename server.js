19ed000a36d9889c19a5cd642157c1f2c289c6c8
veerarrabdullah786 <31401798+veerarrabdullah786@users.noreply.github.com>
2017-10-12 12:44:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article-one', function (req, res) {
+app.get('article-one', function (req, ,res) {
     res.send('Article one are requested and will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
